88c55f2d5c6e3434489a6fe146d1591cc51bc441
hussainirfas <hussainirfas@gmail.com>
2022-11-04 10:07:12 +0530

    Completed project

--- src/components/btn.js
+++ src/components/btn.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 export default function Btn({label, amount}) {
+
+  const dispatch = useDispatch()
+
+  function handleSubmit(){
+    if(label==='EMI'){
+      dispatch({type: 'Spend', payload: amount})
+    } else if(label==='Salary'){
+      dispatch({type: 'Receive', payload: amount})
+    }
+  }
+
   return (
     <div className='flex flex-col justify-between h-36 w-80 rounded-md p-4 bg-slate-500'>
         <div className='flex justify-between'>
@@ -8,7 +20,7 @@ export default function Btn({label, amount}) {
             <span>{amount}</span>
         </div>
         <div className='flex justify-center'>
-            <button className='p-2 h-10 w-32 bg-slate-200 rounded-md'>Submit</button>
+            <button className='p-2 h-10 w-32 bg-slate-200 rounded-md' onClick={handleSubmit}>Submit</button>
         </div>
     </div>
   )
--- src/components/editamount.js
+++ src/components/editamount.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function Editamount ({label}) {
     const [val, setVal] = useState(0);
 
+    const dispatch = useDispatch();
+
     function handleChange(e) {
         setVal(e.target.value)
+    };
+
+    function handleSubmit() {
+      if (label==='Spend') {
+        dispatch({type: 'Spend', payload: val})
+        setVal('')
+      }
+
+      if (label==='Receive') {
+        dispatch({type: 'Receive', payload: val})
+        setVal('')
+      }
     };
 
   return (
@@ -13,7 +28,7 @@ export default function Editamount ({label}) {
         <span className='text-center'>{label}</span>
         <input className='p-4 h-10' type="number" min="1" max="2000000" value={val} onChange={handleChange}/>
         <div className='flex justify-center'>
-            <button className='p-2 h-10 w-32 bg-slate-200 rounded-md'>Submit</button>
+            <button className='p-2 h-10 w-32 bg-slate-200 rounded-md' onClick={handleSubmit}>Submit</button>
         </div>
     </div>
   )
